798f6bf12c85eac5db2a44df6c322676653aa5f2
Nei Barros <nei_webdev@hotmail.com>
2019-06-12 20:42:57 -0300

    add and config mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://semana:semana7@cluster0-zzexb.mongodb.net/test?retryWrites=true&w=majority', {
+   useNewUrlParser: true
+});
 
 app.get('/', (req, res) => {
     return res.send(`Olá ${req.query.name}`);
