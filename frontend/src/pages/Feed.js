0221c468044c31ddb9959c176606af9981a48b93
Nei Barros <jlantonio86@gmail.com>
2019-06-14 14:15:13 -0300

    Get feed data from api

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 
 import './Feed.css';
 
@@ -8,64 +9,48 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component {
+    state = {
+        feed: [],
+    };
+
+    async componentDidMount() {
+        const response = await api.get('posts');
+
+        this.setState({ feed: response.data });
+    }
+
     render() {
         return (
            <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Nei Barros</span>
-                            <span className="place">Mauá</span>
-                        </div>
+                { this.state.feed.map(post => (
+                    <article>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
 
-                        <img src={more} alt="Mais" />
-                    </header>
+                            <img src={more} alt="Mais" />
+                        </header>
 
-                    <img src="http://localhost:3333/files/nei.jpg" alt="" />
+                        <img src={`http://localhost:3333/files/${post.image}`} alt="" />
 
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="Curtir" />
-                            <img src={comment} alt="Comentar" />
-                            <img src={send} alt="Enviar" />
-                        </div>
+                        <footer>
+                            <div className="actions">
+                                <img src={like} alt="Curtir" />
+                                <img src={comment} alt="Comentar" />
+                                <img src={send} alt="Enviar" />
+                            </div>
 
-                        <strong>900 curtidas</strong>
+                            <strong>{post.likes} curtidas</strong>
 
-                        <p>
-                            Um post muito massa da OmniStack!
-                            <span>#react #omnistack #top</span>
-                        </p>
-                    </footer>
-                </article>
-
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Nei Barros</span>
-                            <span className="place">Mauá</span>
-                        </div>
-
-                        <img src={more} alt="Mais" />
-                    </header>
-
-                    <img src="http://localhost:3333/files/nei.jpg" alt="" />
-
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="Curtir" />
-                            <img src={comment} alt="Comentar" />
-                            <img src={send} alt="Enviar" />
-                        </div>
-
-                        <strong>900 curtidas</strong>
-
-                        <p>
-                            Um post muito massa da OmniStack!
-                            <span>#react #omnistack #top</span>
-                        </p>
-                    </footer>
-                </article>
+                            <p>
+                                {post.description}
+                                <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
+                )) }
            </section>
         );
     }
