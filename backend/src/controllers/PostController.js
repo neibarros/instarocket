d79bfe44a243f93b237a101f36f1755f12eaf879
Nei Barros <jlantonio86@gmail.com>
2019-06-14 10:35:42 -0300

    Create methods, make a calls to api and make a upload of image

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -2,10 +2,22 @@ const Post = require('../models/Post');
 
 module.exports = {
     async index(req, res) {
-
+        const post = await Post.find().sort('-createdAt');
+        return res.json(post);
     },
 
     async store(req, res) {
-        
+        const { author, place, description, hashtags } = req.body;
+        const { filename: image } = req.file;
+
+        const post = await Post.create({
+            author,
+            place,
+            description,
+            hashtags,
+            image,
+        });
+
+        return res.json(post);
     }
 };
